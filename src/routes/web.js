2dae919c94e641b254c2f16c142e998a8a8c52d9
khlissaz <khlissaz@yahoo.fr>
2020-09-02 11:04:17 +0100

    update webhook

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,12 @@
 import express from "express";
 import homepageController from "../controllers/homepageController"
+import chatbotController from "../controllers/chatBotController";
+
 let router= express.Router();
 let initWebRoutes= (app)=>{
  router.get("/",homepageController.getHomepage);
-
+ router.get("/webhook",chatbotController.getWebhook);
+ router.post("/webhook",chatbotController.postWebhook);
  return app.use("/",router);
 
 };
