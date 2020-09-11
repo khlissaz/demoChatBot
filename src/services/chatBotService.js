5d31f7eb0fa50540c0ad42f784bdd7d896eb3f3f
khlissaz <khlissaz@yahoo.fr>
2020-09-11 15:26:00 +0100

    update

--- src/services/chatBotService.js
+++ src/services/chatBotService.js
@@ -4,7 +4,7 @@ import request from "request";
 //const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 let getFacebookUsername = (sender_psid) => {
-    return new Promise(function(resolse, reject)  {
+    return new Promise(function(resolve, reject)  {
         // Send the HTTP request to the Messenger Platform
         let uri = "https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}";
 
