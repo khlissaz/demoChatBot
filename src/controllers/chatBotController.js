e4fcfa7170de215dcc089a979daf3f99f359ea0f
khlissaz <khlissaz@yahoo.fr>
2020-09-13 15:02:57 +0100

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -142,6 +142,7 @@ let handlePostback = (sender_psid, received_postback) => {
       //get username
       let username = chaBotService.getFacebookUsername(sender_psid).then(function (res) { console.log("username is" + res) });
       console.log("+++" + username);
+      chaBotService.sendMainMenu(sender_psid);//
       chaBotService.sendResponseWelcomeNewCustomer(username, sender_psid);
       // response = { "text": "سلام {username}$، كيفاه نجمو نعاونوك؟" };
      
