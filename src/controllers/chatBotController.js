25e4d182ad0727d48689110bfa99ef04310377cf
khlissaz <khlissaz@yahoo.fr>
2020-09-16 14:58:41 +0100

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -139,10 +139,10 @@ function firstEntity(nlp, name) {
 // Handles messages events
 let handleMessage = async (sender_psid, message) => {
   //checking quick reply
-  if (user.modele != null ) {
+  if (user.modele != null && user.panne == "") {
     console.log("111111"+user.name);
     await chatBotService.sendMessageAskingPanne(sender_psid);
-    console.log("2222222"+user.name);
+    console.log("2222222"+message.text);
     user.panne = message.text;
     console.log("2222222"+user.panne);
   } else if (user.modele != null && user.panne != null && user.phoneNumber == null) {
