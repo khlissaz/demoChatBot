346549a0bc444ad86faee7a1e0c1d5b98345cfe0
khlissaz <khlissaz@yahoo.fr>
2020-09-21 10:53:18 +0100

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -79,7 +79,25 @@ let getWebhook = (req, res) => {
     }
   }
 };
-
+let getFacebookUsername = (sender_psid) => {
+  return new Promise((resolve, reject) => {
+      // Send the HTTP request to the Messenger Platform
+      let uri = `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`;
+      request({
+          "uri": uri,
+          "method": "GET",
+      }, (err, res, body) => {
+          if (!err) {
+              //convert string to json object
+              body = JSON.parse(body);
+              let username = `${body.last_name} ${body.first_name}`;
+              resolve(username);
+          } else {
+              reject("Unable to send message:" + err);
+          }
+      });
+  });
+};
 
 /*let handleMessageWithEntities = (message) => {
   let entitiesArr = ["datetime", "phone_number"];
