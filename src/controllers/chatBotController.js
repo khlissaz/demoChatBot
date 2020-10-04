0f234e8c597e543184f3418df49ee1fb55ee0156
khlissaz <khlissaz@yahoo.fr>
2020-10-04 14:15:06 +0100

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -161,7 +161,7 @@ let handleMessage = async (sender_psid, message) => {
 
   //handle text message
   let entity = handleMessageWithEntities(message);
-  console.log(entity+ "**++++++*****")
+  console.log(entity.JSON+ "**++++++*****")
   console.log(message.data+ "*******")
   console.log(message.nlp.traits.object+ "*****+++++")
   /* if (entity.name === "phone_number") {
